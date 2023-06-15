dc55a327e604244a5c7939189c979c7e78f7ec98
ImmaBun <dominianalphaz69@gmail.com>
2023-06-15 21:06:27 +0700

    checkboxes

--- molCalc.py
+++ molCalc.py
@@ -66,20 +66,29 @@ def Calculate():
     else:
         value = (value/current_unit_type.converfactor)
 
+    # ! Horrendous code lol
     # Convert moles into each unit type and append to OUTPUT_VALUES list
     for unittype in RADIOBUTTON_OPTIONS:
-        if unittype.converfactor == None:
-            if unittype.name == "Mass":
-                value_new = (value*altput_value1)
-            else:
-                value_new = (value*altput_value2)
-        else:
+        if unittype.converfactor == None:                   # If mass or concentration
+            if unittype.name == "Mass":                         # If mass
+                if state_checkbox1.get() == 1:                      # If swapped values for mass, the formula is reversed
+                    value_new = (altput_value1/value)
+                else:
+                    value_new = (value*altput_value1)
+            else:                                               # If concentration
+                if state_checkbox2.get() == 1:                      # If swapped values for concentration, the formula is reversed
+                    value_new = (altput_value2/value)
+                else:                                       
+                    value_new = (value*altput_value2)
+        else:                                               # Otherwise, carry on by multiplying with its conversion factor
             value_new = (value*unittype.converfactor)
         value_new = round(value_new, 4)
         OUTPUT_VALUES.append(value_new)
 
     ShowOutput(OUTPUT_VALUES)
     
+
+
 def ShowOutput(OUTPUT_VALUES):
     # Inserts output from OUTPUT_VALUES into each entry
     for entry in OUTPUT_ENTRIES:
@@ -87,9 +96,7 @@ def ShowOutput(OUTPUT_VALUES):
         entry["state"] = "normal"
         entry.delete(0, tk.END)
         entry.insert(0, OUTPUT_VALUES[entry_index])
-        entry["state"] = "readonly"
-
-        
+        entry["state"] = "readonly"    
 
 
 # When a Radio Button is pressed in the input
@@ -126,13 +133,78 @@ def SetUnitType():
     input_unit_alt = tk.Label(input_frame, text=current_unit_type.alt_unit, width=10, anchor="w")
     input_unit_alt.grid(row=2, column=4, sticky="w")
 
-def ToggleAltValue():
-    pass
+
+# Checkbox1 swaps g and g/mol
+def ToggleAltValueOne():
+    global output_unit_g
+    global output_label_alt1
+    global output_entry_g
+    global output_entry_alt1
+
+    output_unit_g.destroy()
+    output_label_alt1.destroy()
+
+    if state_checkbox1.get() == 1:
+        output_unit_g = tk.Label(output_frame, text="g/mol", width=11, anchor="w")
+        output_label_alt1= tk.Label(output_frame, text="g", width=5, anchor="w")
+    else:
+        output_unit_g = tk.Label(output_frame, text="g", width=11, anchor="w")
+        output_label_alt1= tk.Label(output_frame, text="g/mol", width=5, anchor="w")
+
+    # Swapping the values from both entries
+    output_entry_g["state"] = "normal"
+    
+    intermediate_val = output_entry_g.get()
+    output_entry_g.delete(0, tk.END)
+    output_entry_g.insert(0, output_entry_alt1.get())
+    output_entry_alt1.delete(0, tk.END)
+    output_entry_alt1.insert(0, intermediate_val)
+
+    output_entry_g["state"] = "readonly"
+
+    output_unit_g.grid(row=1, column=2, sticky="w")
+    output_label_alt1.grid(row=1, column=4, sticky="w")
+
+
+
+
+def ToggleAltValueTwo():
+    global output_unit_cv
+    global output_label_alt2
+    global output_entry_cv
+    global output_entry_alt2
+
+    output_unit_cv.destroy()
+    output_label_alt2.destroy()
+
+    if state_checkbox2.get() == 1:
+        output_unit_cv = tk.Label(output_frame, text="L", width=11, anchor="w")
+        output_label_alt2= tk.Label(output_frame, text="mol/L", width=5, anchor="w")
+    else:
+        output_unit_cv = tk.Label(output_frame, text="mol/L", width=11, anchor="w")
+        output_label_alt2= tk.Label(output_frame, text="L", width=5, anchor="w")
+
+    # Swapping the values from both entries
+    output_entry_cv["state"] = "normal"
+    
+    intermediate_val = output_entry_cv.get()
+    output_entry_cv.delete(0, tk.END)
+    output_entry_cv.insert(0, output_entry_alt2.get())
+    output_entry_alt2.delete(0, tk.END)
+    output_entry_alt2.insert(0, intermediate_val)
+
+    output_entry_cv["state"] = "readonly"
+
+    output_unit_cv.grid(row=4, column=2, sticky="w")
+    output_label_alt2.grid(row=4, column=4, sticky="w")
+
+
+
 
 # Defining the units in the UnitType class, then putting them into RADIOBUTTON_OPTIONS list so its index can be accessed by the For Loop
 Unit_mol = UnitType("Moles", "mol", None, None, 1)
 Unit_g = UnitType("Mass", "g", "Molecular Weight", "g/mol", None)
-Unit_v = UnitType("Volume", "L @ STP", None, None, 22.4)
+Unit_v = UnitType("Volume", "L (@STP)", None, None, 22.4)
 Unit_n = UnitType("Particles", "x10^23 atoms", None, None, 6.02)
 Unit_cv = UnitType("Concentration", "mol/L", "Volume", "L", None)
 
@@ -164,6 +236,13 @@ output_entry_v = tk.Entry(output_frame, width=10, state="readonly")
 output_entry_n = tk.Entry(output_frame, width=10, state="readonly")
 output_entry_cv = tk.Entry(output_frame, width=10, state="readonly")
 
+# Labels for units in Output Frame
+output_unit_mol = tk.Label(output_frame, text="mol", width=11, anchor="w")
+output_unit_g = tk.Label(output_frame, text="g", width=11, anchor="w")
+output_unit_v = tk.Label(output_frame, text="L(@STP)", width=11, anchor="w")
+output_unit_n = tk.Label(output_frame, text="x10^23 atoms", width=11, anchor="w")
+output_unit_cv = tk.Label(output_frame, text="mol/L", width=11, anchor="w")
+
 # Additional Labels and Entries for AltValues in Output Frame
 output_entry_alt1 = tk.Entry(output_frame, width=10)
 output_entry_alt2 = tk.Entry(output_frame, width=10)
@@ -178,8 +257,8 @@ state_checkbox1.set(0)
 state_checkbox2 = tk.IntVar()
 state_checkbox2.set(0)
 
-output_checkbox1 = tk.Checkbutton(output_frame, text="Swap", variable=state_checkbox1, onvalue=1, offvalue=0, command=lambda: ToggleAltValue("Box1"))
-output_checkbox2 = tk.Checkbutton(output_frame, text="Swap", variable=state_checkbox2, onvalue=1, offvalue=0, command=lambda: ToggleAltValue("Box1"))
+output_checkbox1 = tk.Checkbutton(output_frame, text="Swap", variable=state_checkbox1, onvalue=1, offvalue=0, command=ToggleAltValueOne)
+output_checkbox2 = tk.Checkbutton(output_frame, text="Swap", variable=state_checkbox2, onvalue=1, offvalue=0, command=ToggleAltValueTwo)
 
 
 OUTPUT_ENTRIES = [output_entry_mol, output_entry_g, output_entry_v, output_entry_n, output_entry_cv]
@@ -213,6 +292,12 @@ output_entry_v.grid(row=2, column=1)
 output_entry_n.grid(row=3, column=1)
 output_entry_cv.grid(row=4, column=1)
 
+output_unit_mol.grid(row=0, column=2, sticky="w")
+output_unit_g.grid(row=1, column=2, sticky="w")
+output_unit_v.grid(row=2, column=2, sticky="w")
+output_unit_n.grid(row=3, column=2, sticky="w")
+output_unit_cv.grid(row=4, column=2, sticky="w")
+
 output_entry_alt1.grid(row=1, column=3)
 output_entry_alt2.grid(row=4, column=3)
 
@@ -225,7 +310,6 @@ output_label_alt2.grid(row=4, column=4)
 
 for unittype in RADIOBUTTON_OPTIONS:
     tk.Label(output_frame, text=f"{unittype.name}: ", width=15, anchor="w").grid(row=RADIOBUTTON_OPTIONS.index(unittype), column=0)
-    tk.Label(output_frame, text=unittype.unit, width=11, anchor="w").grid(row=RADIOBUTTON_OPTIONS.index(unittype), column=2)
 
 # White Space
 tk.Label(input_frame, text=" ").grid(row=3, column=0)
